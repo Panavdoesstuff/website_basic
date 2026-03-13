d66b5ac6fdc8ca0ba8483bcfb59ad2455b03b00a
Panav RG <panavrg@Panavs-MacBook-Air.local>
2026-03-13 15:08:40 +0530

    surprise me function

--- script.js
+++ script.js
@@ -3,3 +3,12 @@ function rollDice()
     let num = Math.floor(Math.random()*6) + 1;
     document.getElementById("result").textContent = num;
 }
+
+function funfunc()
+{
+    let number = Math.floor(Math.random()*3) + 1;
+    if(number==1) emoji = ":)";
+    else if(number==2) emoji = "<3";
+    else if(number==3) emoji = "67";
+    document.getElementById("res").textContent = emoji;
+}
